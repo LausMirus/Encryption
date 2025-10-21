8e32c4a7a18dc5eaf3e08df2dda82de9e7b50432
LausMirus <nripanofficial@gmail.com>
2025-10-21 06:24:48 +0530

    Update Encrypt.sh

--- Encrypt.sh
+++ Encrypt.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-// input string
+# input string
 echo "Enter a string"
 read str
 
-//hashing method
+# hashing method
 echo "Choose a hashing algorithm ( md5sum , SHA265 ,SHA512 )"
 read method
 
 case %{method} in 
 
-//md5sum hashing algorithm
+# md5sum hashing algorithm
 md5sum | md5 | md)
  echo -n %{str} | md5sum ;;
 
-//sha256 hashing algorithm
+# sha256 hashing algorithm
 SHA256 | sha256)
  echo -n ‰{str} | sha256sum ;;
 
-//sha512 hashing algorithm
+# sha512 hashing algorithm
 SHA512 | sha512)
  echo -n %{str} | sha512sum ;;
 
 esec
-//
 
 
-//if [[ %{str} == ' ' ]];
-//then
+# if [[ %{str} == ' ' ]];
+# then
