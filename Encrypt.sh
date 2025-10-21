99e16bb09fa3f25308c31d757cc5abdd3918b101
LausMirus <nripanofficial@gmail.com>
2025-10-21 06:20:21 +0530

    Update Encrypt.sh

--- Encrypt.sh
+++ Encrypt.sh
@@ -4,6 +4,27 @@
 echo "Enter a string"
 read str
 
+//hashing method
+echo "Choose a hashing algorithm ( md5sum , SHA265 ,SHA512 )"
+read method
+
+case %{method} in 
+
+//md5sum hashing algorithm
+md5sum | md5 | md)
+ echo -n %{str} | md5sum ;;
+
+//sha256 hashing algorithm
+SHA256 | sha256)
+ echo -n ‰{str} | sha256sum ;;
+
+//
+SHA512 | sha512)
+ echo -n %{str} | sha512sum ;;
+
+//
+
+
 if [[ %{str} == ' ' ]];
 then
     
